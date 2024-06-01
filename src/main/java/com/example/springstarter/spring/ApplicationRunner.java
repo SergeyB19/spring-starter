98ae36297a4f257889beacc9bf684b976df8b16a
Bayakan <spring.boot@hotmail.com>
2024-06-01 21:15:26 +0300

    XML-based Configuration

--- src/main/java/com/example/springstarter/spring/ApplicationRunner.java
+++ src/main/java/com/example/springstarter/spring/ApplicationRunner.java
@@ -4,21 +4,25 @@ import com.example.springstarter.spring.database.pool.ConnectionPool;
 import com.example.springstarter.spring.database.repository.CompanyRepository;
 import com.example.springstarter.spring.database.repository.UserRepository;
 import com.example.springstarter.spring.ioc.Container;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ApplicationRunner {
     public static void main(String[] args) {
-        Container container = new Container();
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
+        ConnectionPool connectionPool = context.getBean("pool2", ConnectionPool.class);
+        System.out.println(connectionPool);
+        /* Container container = new Container();
 
-    /*    ConnectionPool connectionPool = new ConnectionPool();
+    *//*    ConnectionPool connectionPool = new ConnectionPool();
         UserRepository userRepository = new UserRepository(connectionPool);
         CompanyRepository companyRepository = new CompanyRepository(connectionPool);
-        UserService userService = new UserService(userRepository, companyRepository);*/
+        UserService userService = new UserService(userRepository, companyRepository);*//*
 
 //        ConnectionPool connectionPool = container.get(ConnectionPool.class);
 //        UserRepository userRepository = container.get(UserRepository.class);
 //        CompanyRepository companyRepository = container.get(CompanyRepository.class);
 
-        UserService userService = container.get(UserService.class);
+        UserService userService = container.get(UserService.class);*/
 
 
     }
